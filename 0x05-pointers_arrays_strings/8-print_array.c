124135e5cf28b31bbf2238098eabccd97b6ea8f2
graceamole <gracey1235@gmail.com>
2023-03-01 03:39:42 -0800

    print n elements of a new array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: array
+ * @n: number of values to be printed
+ */
+void print_array(int *a, int b)
+{
+	int i;
+
+	for (i = 0; i < b; i++)
+	{
+		printf("%d", a[i]);
+		if (i != b - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
