c98f906eb16f2d8379b4fd1d713425e1cb181592
graceamole <gracey1235@gmail.com>
2023-03-01 03:43:30 -0800

    print n elements of a new array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
  * print_array - prints n elements of an array
  * @a: array
- * @n: number of values to be printed
+ * @b: number of values to be printed
  */
 void print_array(int *a, int b)
 {
